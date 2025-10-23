ea31096ba5783ae68f7aef8a5e86acacddcb398b
marks <16828892+Mark-CLi@users.noreply.github.com>
2025-10-23 16:06:39 -0700

    done

--- js-object-manipulation/sculptureScript.js
+++ js-object-manipulation/sculptureScript.js
@@ -8,5 +8,15 @@ const getStringLengths = (obj) =>
 
 const sculptureListLengths = sculptureList.map(getStringLengths);
 
+// it aks for a loop, i guess the map is technically a loop but here is a for loop method
+// personally i liek to use map more since all the js framework stuff i do.
+// for (const sculpture of sculptureList) {
+//     const lengthsObject = {};
+//     for (const key in sculpture) {
+//         lengthsObject[key] = sculpture[key].length;
+//     }
+//     sculptureListLengths.push(lengthsObject);
+// }
+
 console.log('sculptureListLengths:');
 console.log(JSON.stringify(sculptureListLengths, null, 2));
