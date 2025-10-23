9bb20a75935a22798f6391494fdc22a905f9d101
Mark <16828892+Mark-CLi@users.noreply.github.com>
2025-10-23 16:33:13 -0700

    Update sculptureScript.js
    using object,assign and loop

--- js-object-manipulation/sculptureScript.js
+++ js-object-manipulation/sculptureScript.js
@@ -1,22 +1,21 @@
 const sculptureList = require('./data.js'); // import data.js
 
 // const element = sculptureList[0] // <---- comment out this line in your solution!
-const getStringLengths = (obj) =>
-    Object.fromEntries(
-        Object.entries(obj).map(([key, value]) => [key, value.length])
-    );
 
-const sculptureListLengths = sculptureList.map(getStringLengths);
+const sculptureListLengths = [];
 
-// it aks for a loop, i guess the map is technically a loop but here is a for loop method
-// personally i liek to use map more since all the js framework stuff i do uses map more than  loop.
-// for (const sculpture of sculptureList) {
-//     const lengthsObject = {};
-//     for (const key in sculpture) {
-//         lengthsObject[key] = sculpture[key].length;
-//     }
-//     sculptureListLengths.push(lengthsObject);
-// }
+for (const item of sculptureList) {
+    const lengths = {};
+    for (const k in item) {
+        Object.assign(lengths, { [k]: item[k].length });
+    }
+    sculptureListLengths.push(lengths);
+}
+
+// I probably would use map, but the objetive ask for loop
+// const sculptureListLengths = sculptureList.map(obj =>
+//     Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, v.length]))
+// );
 
 console.log('sculptureListLengths:');
 console.log(JSON.stringify(sculptureListLengths, null, 2));
