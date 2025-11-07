7ead268e650f8aa8ba0a09365eb48b39696b5421
marks <16828892+Mark-CLi@users.noreply.github.com>
2025-11-06 19:43:07 -0800

    rdy

--- dict-api/index.js
+++ dict-api/index.js
@@ -5,7 +5,6 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
-app.use("/node_modules", express.static("node_modules"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
